aa83e2244abf17d93bf60072771413366b09e6ff
fiennyangeln <fifi.angelina@yahoo.com>
2018-02-17 22:39:26 +0800

    add project admin

--- oh_proj_management/urls.py
+++ oh_proj_management/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import HomeView, LoginView
+from project_admin.views import HomeView, LoginView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
