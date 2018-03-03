ec123cb2fa1db46e587a31c8260091f747e78e4e
rosygupta <rosy01.gupta@gmail.com>
2018-03-03 12:54:05 +0530

    logout added

--- oh_proj_management/urls.py
+++ oh_proj_management/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 
-from project_admin.views import HomeView, LoginView
+from project_admin.views import HomeView, LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home'),
+    path('', login_required(HomeView.as_view(), login_url='login/'), name='home'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name= 'logout')
 ]
