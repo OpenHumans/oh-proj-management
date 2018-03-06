534f076d329eec243a9db98c8a703d7bc7e2738d
Mike Escalante <mcescalante@gmail.com>
2018-03-06 15:21:56 -0500

    adjust secret key for travis

--- oh_proj_management/settings.py
+++ oh_proj_management/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'testkeyifnone')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = to_bool(os.getenv('DEBUG'), default=False)
