f08f619592a752933fe05f433afd728b17135a81
Shubhangi Chauhan <36580419+shubhangi-chauhan@users.noreply.github.com>
2018-03-06 22:00:57 +0530

    Add members list and use bootstrap (#34)
    * Add members detail list and use bootstrap

--- project_admin/forms.py
+++ project_admin/forms.py
@@ -2,4 +2,6 @@ from django import forms
 
 
 class TokenForm(forms.Form):
-    token = forms.CharField()
+    token = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
