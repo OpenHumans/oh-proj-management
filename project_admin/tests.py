77bfdad306d6b3f6d43fd1e8064b637b8476fffa
vaatiesther <vaatiesther@gmail.com>
2018-03-15 21:25:21 +0300

    update tests to use vcrpy package

--- project_admin/tests.py
+++ project_admin/tests.py
@@ -1,19 +1,17 @@
 from django.test import TestCase
-from django.test import Client
 import requests
-import requests_mock
+import vcr
 
 
 class ProjectTest(TestCase):
 
     def setUp(self):
-        self.client = Client()
         self.token = 'XitlFDXBqm5TRK8Vuh3Ey2cDFdiTWz7amKpot97H9Xfgak1qpvray0b0arQhvpEP'
         self.url = 'https://www.openhumans.org/api/direct-sharing/project/?access_token={}'.format(
             self.token)
 
-    def test_member_list_view(self):
-        with requests_mock.Mocker() as mock:
-            response = mock.get(self.url)
-            response = requests.get(self.url)
-            self.assertEqual(response.status_code, 200)
+    @vcr.use_cassette('members.yml')
+    def test_stupid_request(self):
+        """memebers list API"""
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
