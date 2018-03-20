b53d572e0b00d71ab39fb7468961891447034a5d
essykings <essyking@gmail.com>
2018-03-20 09:39:50 +0300

    fix PEP8 errors

--- project_admin/tests.py
+++ project_admin/tests.py
@@ -4,11 +4,10 @@ import vcr
 
 
 class ProjectTest(TestCase):
-
     def setUp(self):
         self.token = 'XitlFDXBqm5TRK8Vuh3Ey2cDFdiTWz7amKpot97H9Xfgak1qpvray0b0arQhvpEP'
         self.url = 'https://www.openhumans.org/api/direct-sharing/project/members/?access_token={}'.format(
-        	self.token)
+                    self.token)
 
     @vcr.use_cassette('tests/members.yml')
     def test_members_request(self):
