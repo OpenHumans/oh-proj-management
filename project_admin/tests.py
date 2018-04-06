e21ccaec54b395a84467f9cd8f44fecf9fc704e1
vaatiesther <essyking@gmail.com>
2018-04-06 06:56:58 +0300

    members list tests (#36)
    * members list tests with vcrpy
    * update member tests
    * remove access tokens and replace with generic string, add login_success cassette

--- project_admin/tests.py
+++ project_admin/tests.py
@@ -4,6 +4,20 @@ import requests
 from django.urls import reverse
 from django.conf import settings
 
+
+class ProjectTest(TestCase):
+    def setUp(self):
+        self.token = 'XitlFDXBqm5TRK8Vuh3Ey2cDFdiTWz7amKpot97H9Xfgak1qpvray0b0arQhvpEP'
+        self.url = 'https://www.openhumans.org/api/direct-sharing/project/members/?access_token={}'.format(
+                    self.token)
+
+    @vcr.use_cassette('tests/members.yml')
+    def test_members_request(self):
+        """memebers list API"""
+        response = requests.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+
 my_vcr = vcr.VCR(path_transformer=vcr.VCR.ensure_suffix('.yaml'), cassette_library_dir='project_admin/cassettes')
 
 
