4976d291d021432f2dce203d2b5a34b356300571
rosygupta <rosy01.gupta@gmail.com>
2018-07-17 23:20:36 +0530

    changed choicefilter to charfilter

--- project_admin/filter.py
+++ project_admin/filter.py
@@ -10,20 +10,12 @@ def groups_joined(request):
     return project.projectgroup_set.all()
 
 
-def number_files_shared():
-    project_members = ProjectMember.objects.all()
-    file_numbers = project_members.values_list(
-        'file_count', flat=True)
-    file_numbers = tuple((value, value) for value in set(file_numbers))
-    return list(file_numbers)
-
-
 class MemberFilter(django_filters.FilterSet):
     id = django_filters.CharFilter(name='id', lookup_expr='iexact', label='Member ID')
     date_joined = django_filters.DateTimeFilter(name='date_joined', lookup_expr='gt',
                                                 widget=forms.DateInput(attrs={'type': 'date', 'class': 'datepicker'}),)
     groups = django_filters.ModelChoiceFilter(label='Groups Joined', queryset=groups_joined)
-    file_count = django_filters.ChoiceFilter(label='Number of Files Shared', choices=number_files_shared())
+    file_count = django_filters.CharFilter(label='Number of Files Shared')
     sources_shared = django_filters.CharFilter(label='Sources Authorized')
 
     class Meta:
