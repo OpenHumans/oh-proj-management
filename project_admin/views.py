63726a845561ee1bee101f9e9b87247ee0fd63d3
Ba <ba@Athinas-MBP.dhcp.lbl.gov>
2018-08-23 15:37:26 -0700

    whops, renamed function

--- project_admin/views.py
+++ project_admin/views.py
@@ -10,7 +10,7 @@ from .forms import TokenForm
 from .models import Project, ProjectMember, ProjectGroup
 from .filter import MemberFilter
 from .tasks import download_zip_files, update_project_members
-from .helpers import get_all_members, update_members, paginate_items, is_token_valid
+from .helpers import get_all_members, update_members, paginate_items, token_is_valid
 
 
 class HomeView(ListView):
