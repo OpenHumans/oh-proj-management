36c9195935546b7f8670814847f059f49f9507ac
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-11-06 15:29:17 -0800

    fix whitenoise

--- oh_proj_management/settings.py
+++ oh_proj_management/settings.py
@@ -80,12 +80,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-MIDDLEWARE_CLASSES = [
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware'
-]
-
 ROOT_URLCONF = 'oh_proj_management.urls'
 
 TEMPLATES = [
