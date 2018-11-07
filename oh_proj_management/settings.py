de69866d8fb3a98e507d9bb5cfe978bc189ab3d6
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-11-07 14:26:06 -0800

    in case this matters…

--- oh_proj_management/settings.py
+++ oh_proj_management/settings.py
@@ -161,12 +161,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
