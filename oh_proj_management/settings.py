c2a211f99ca42d2da055644eee655ce083bb3468
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-11-07 14:06:08 -0800

    the order fuckin matters…

--- oh_proj_management/settings.py
+++ oh_proj_management/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,7 +79,6 @@ MIDDLEWARE = [
 
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'oh_proj_management.urls'
