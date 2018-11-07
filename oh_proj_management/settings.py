0c2cfb84108358adb261d6733a000be3ddeb5d31
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-11-07 13:04:55 -0800

    correct static link

--- oh_proj_management/settings.py
+++ oh_proj_management/settings.py
@@ -166,7 +166,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
